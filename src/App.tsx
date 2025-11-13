aece07b22ef0f07d0e95f6ee5734b9d199eeff7f
k12Cyborg <martincelis1221@gmail.com>
2025-11-12 23:00:29 -0300

    archivos iniciales

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+      <div>
+        pagina en construccion uwu
+      </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
+
+import './index.css'
+import App from './App.tsx'
+import { ErrorBoundary } from './ErrorBoundary.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    ErrorBoundary: ErrorBoundary
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
